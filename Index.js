11d81c03b061b0ebd82ed4628cecb66f17052a63
titmuny <sombo.titmuny@gmail.com>
2025-07-19 10:35:38 +0700

    Initial commit

---
+++ Index.js
@@ -0,0 +1,22 @@
+
+AOS.init();
+
+var swiper = new Swiper(".mySwiper", {
+slidesPerView: 4,
+spaceBetween: 20,
+slidesPerGroup: 4,
+loop: false,
+navigation: {
+  nextEl: ".swiper-button-next",
+  prevEl: ".swiper-button-prev",
+},
+breakpoints: {
+  0: { slidesPerView: 1, slidesPerGroup: 1 },
+  576: { slidesPerView: 2, slidesPerGroup: 2 },
+  768: { slidesPerView: 3, slidesPerGroup: 3 },
+  992: { slidesPerView: 4, slidesPerGroup: 4 }
+}
+});
+
+cartItems.push(newItem);
+localStorage.setItem("cartItems", JSON.stringify(cartItems));
